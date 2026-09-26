fc25eddf0fabaab231ecc89f4aa7bba446ca2166
AntoninMignotPilon <antonin.mignotpilon@viacesi.fr>
2026-09-26 17:10:02 +0200

    fix the provider with proxy

--- src/provider.js
+++ src/provider.js
@@ -1,24 +1,31 @@
-const url = "https://query1.finance.yahoo.com/v8/finance/chart/"
-const place = ".PA"
-
 export async function getPrice(ticker) {
-    const targetUrl = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}.PA`;
+    const maxTentatives = 5;
 
+    const targetUrl = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}.PA`;
     const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(targetUrl)}`;
 
-    const response = await fetch(proxyUrl);
-    if (!response.ok) throw new Error(`Erreur Proxy: ${response.status}`);
+    for (let tentative = 1; tentative <= maxTentatives; tentative++) {
+        try {
+            const response = await fetch(proxyUrl);
+            if (!response.ok) throw new Error(`Erreur Proxy: ${response.status}`);
 
-    const proxyData = await response.json();
+            const proxyData = await response.json();
+            const yahooData = JSON.parse(proxyData.contents);
 
-    const yahooData = JSON.parse(proxyData.contents);
+            const result = yahooData.chart.result[0];
+            const meta = result.meta;
 
-    const result = yahooData.chart.result[0];
-    const meta = result.meta;
-
-    return {
-        price: meta.regularMarketPrice,
-        previousClose: meta.chartPreviousClose,
-        shortName: meta.shortName || ticker
-    };
+            return {
+                price: meta.regularMarketPrice,
+                previousClose: meta.chartPreviousClose,
+                shortName: meta.shortName || ticker
+            };
+        } catch (error) {
+            if (tentative === maxTentatives) {
+                throw error;
+            }
+            const delai = 1000 * 2 ** (tentative - 1); //1s, 2s, 4s ...
+            await new Promise(r => setTimeout(r, delai));
+        }
+    }
 }
